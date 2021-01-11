fbdb2c591fcfc13bbcdc0125d69cce31440aa1de
Doerthous <827484343@qq.com>
2021-01-11 14:43:27 +0800

    fix: 添加mac地址

--- project/hw/board_enc28j60.c
+++ project/hw/board_enc28j60.c
@@ -8,6 +8,7 @@ enc28j60_t enc28j60 =
 	.cs.pin = GPIO_Pin_7,
 	.cs.mode = GPIO_Mode_Out_PP,
 	.cs.speed = GPIO_Speed_50MHz,
+	.mac_addr = { 0x68, 0x19, 0x78, 0x11, 0x12, 0x34 },
 };
 
 gpio_t enc28j60_int = {
