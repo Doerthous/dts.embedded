0d9350c483f25a49b1bc4295cbf58ca3a8d184e0
Doerthous <827484343@qq.com>
2021-01-30 15:16:40 +0800

    fix: init_thrd_init

--- project/stm32f10x-mdk-freertos_printf - Copy/app/init_thrd.c
+++ project/stm32f10x-mdk-freertos_printf - Copy/app/init_thrd.c
@@ -1,6 +1,7 @@
 #include <dts/embedded/hal/gpio.h>
 #include <dts/embedded/hal/spi.h>
 #include <dts/embedded/hal/interrupt.h>
+#include <dts/embedded/hal/uart.h>
 #include <dts/embedded/lib/delay.h>
 #include <dts/embedded/lib/timer.h>
 #include <dts/embedded/lib/tick.h>
@@ -9,7 +10,7 @@
 extern dts_hal_timer_t tim1;
 extern gpio_t led;
 extern spi_t spi2;
-extern spi_t sw_spi2;
+extern uart_t uart1;
 extern enc28j60_t enc28j60;
 extern void isr_thrd_init(void);
 
@@ -54,9 +55,12 @@ void dts_hal_default_handler(void *_, ...) {}
 
 thrd_t *init_thread;
 
+#include <dts/embedded/hal/lib/uart_printf.h>
 void init_thread_entry(void* parameter)
 {
 	gpio_init(&led);
+	
+	uart_init(&uart1);
 	
 	spi_init(&spi2);
     enc28j60.spi = &spi2;
@@ -71,10 +75,19 @@ void init_thread_entry(void* parameter)
 	while (1) {
 		thrd_sleep(tick_from_second(1));
 		dts_net_sys_heartbeat();
+		uart_printf(&uart1, "World!\n");
 	}
 }
-
+thrd_t *t_thread;
+void t_thrd_entry(void *_)
+{
+	while (1) {
+		thrd_sleep(tick_from_second(1));
+		uart_printf(&uart1, "Hello!\n");
+	}
+}
 void init_thrd_init(void)
 {
 	thrd_new(&init_thread, init_thread_entry, NULL, 1024);
+	thrd_new(&t_thread, t_thrd_entry, NULL, 1024);
 }
